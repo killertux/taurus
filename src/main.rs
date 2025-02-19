b039d55fbec5bbac5428002a635ee8ab00411ba3
Bruno Clemente <bruno.canguru@hotmail.com>
2025-02-19 14:56:18 -0300

    Parsing and propely showing gemtext

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use client::{Client, GeminiResponse};
 use url::Url;
 
@@ -9,7 +9,7 @@ mod client;
 use crossterm::event::{self, Event};
 use ratatui::{
     prelude::{Buffer, Rect},
-    style::Stylize,
+    style::{self, Color, Style, Stylize},
     text::Line,
     widgets::{Block, Paragraph, Widget, Wrap},
     DefaultTerminal, Frame,
@@ -18,7 +18,7 @@ use ratatui::{
 struct App {
     current_url: Url,
     client: Client,
-    content: String,
+    content: Option<Content>,
 }
 
 fn main() -> Result<()> {
@@ -29,13 +29,39 @@ fn main() -> Result<()> {
     result
 }
 
+struct Content {
+    mime: String,
+    body: Body,
+}
+
+enum Body {
+    String(String),
+    Bytes(Vec<u8>),
+}
+
+impl Content {
+    fn from_mime_and_bytes(mime: String, bytes: Vec<u8>) -> Result<Self> {
+        if mime.starts_with("text/") {
+            let body = String::from_utf8(bytes)?;
+            return Ok(Self {
+                mime,
+                body: Body::String(body),
+            });
+        }
+        Ok(Self {
+            mime,
+            body: Body::Bytes(bytes),
+        })
+    }
+}
+
 impl Default for App {
     fn default() -> Self {
         Self {
             current_url: Url::parse("gemini://gemini.circumlunar.space/capcom/")
                 .expect("We know that this is a valid url"),
             client: Client::new(true),
-            content: String::new(),
+            content: None,
         }
     }
 }
@@ -47,10 +73,62 @@ impl Widget for &App {
     {
         let title = Line::from(self.current_url.as_str()).bold();
         let main_block = Block::bordered().title_top(title);
-        Paragraph::new(self.content.as_str())
-            .wrap(Wrap { trim: true })
-            .block(main_block)
-            .render(area, buf);
+        match &self.content {
+            None => Paragraph::new("No content")
+                .wrap(Wrap { trim: true })
+                .block(main_block)
+                .render(area, buf),
+            Some(content) => match &content.body {
+                Body::Bytes(_) => {
+                    Paragraph::new("Format not supported!")
+                        .wrap(Wrap { trim: true })
+                        .block(main_block)
+                        .render(area, buf);
+                }
+                Body::String(body) => {
+                    if content.mime.starts_with("text/gemini") {
+                        let parser = GemTextParser::new(&body, self.current_url.clone());
+                        let mut n_links = 0;
+                        let mut lines = Vec::new();
+                        for line in parser {
+                            let Ok(line) = line else {
+                                dbg!(line.expect_err("Should be an error"));
+                                continue;
+                            };
+                            match line {
+                                GemTextLine::Text(text) => {
+                                    lines.push(Line::raw(text).left_aligned());
+                                }
+                                GemTextLine::PreFormatted(text) => {
+                                    lines.push(
+                                        Line::raw(text)
+                                            .left_aligned()
+                                            .style(Style::new().bg(Color::Gray)),
+                                    );
+                                }
+                                GemTextLine::Link { url, text } => {
+                                    let color = if url.scheme() == "gemini" {
+                                        Color::Blue
+                                    } else {
+                                        Color::Red
+                                    };
+                                    lines.push(Line::styled(
+                                        format!("[{n_links}] {text}"),
+                                        Style::new().fg(color),
+                                    ));
+                                    n_links += 1;
+                                }
+                            }
+                        }
+
+                        Paragraph::new(lines)
+                            .wrap(Wrap { trim: true })
+                            .block(main_block)
+                            .render(area, buf);
+                    }
+                }
+            },
+        }
     }
 }
 
@@ -77,15 +155,69 @@ impl App {
         let response = self.client.request(self.current_url.clone())?;
         match response {
             GeminiResponse::Success { mime, body } => {
-                let content = if mime.starts_with("text") {
-                    String::from_utf8(body)?
-                } else {
-                    format!("Not type that we know how to process. {mime}")
-                };
-                self.content = content;
+                self.content = Some(Content::from_mime_and_bytes(mime, body)?);
             }
             _ => unimplemented!(),
         }
         Ok(())
+    }
+}
+
+struct GemTextParser<'a> {
+    raw_text: &'a str,
+    url: Url,
+}
+
+#[derive(Debug)]
+enum GemTextLine<'a> {
+    Text(&'a str),
+    Link { url: Url, text: &'a str },
+    PreFormatted(&'a str),
+}
+
+impl<'a> GemTextParser<'a> {
+    pub fn new(raw_text: &'a str, url: Url) -> Self {
+        Self { raw_text, url }
+    }
+
+    fn parse_next(&mut self) -> Result<GemTextLine<'a>> {
+        let line = if let Some((line, rest)) = self.raw_text.split_once("\n") {
+            self.raw_text = rest;
+            line
+        } else {
+            let line = self.raw_text;
+            self.raw_text = "";
+            line
+        };
+        if let Some(link_line) = line.strip_prefix("=> ") {
+            let Some((link, text)) = link_line.split_once(|x: char| x.is_whitespace()) else {
+                bail!("Invalid link line {line}");
+            };
+            if !link.contains("://") {
+                return Ok(GemTextLine::Link {
+                    url: self.url.join(link)?,
+                    text,
+                });
+            }
+            return Ok(GemTextLine::Link {
+                url: Url::parse(link)?,
+                text,
+            });
+        }
+        if let Some(pre_formatted_line) = line.strip_prefix("```") {
+            return Ok(GemTextLine::PreFormatted(pre_formatted_line));
+        }
+        Ok(GemTextLine::Text(line))
+    }
+}
+
+impl<'a> Iterator for GemTextParser<'a> {
+    type Item = Result<GemTextLine<'a>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.raw_text.is_empty() {
+            return None;
+        }
+        Some(self.parse_next())
     }
 }
