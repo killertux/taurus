7acd39aab1851772fedaf5f633a2858156f3e296
Bruno Clemente <bruno.canguru@hotmail.com>
2025-02-18 22:09:04 -0300

    Improving tui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use anyhow::Result;
 use client::{Client, GeminiResponse};
 use url::Url;
@@ -5,31 +7,85 @@ use url::Url;
 mod client;
 
 use crossterm::event::{self, Event};
-use ratatui::{DefaultTerminal, Frame};
+use ratatui::{
+    prelude::{Buffer, Rect},
+    style::Stylize,
+    text::Line,
+    widgets::{Block, Paragraph, Widget, Wrap},
+    DefaultTerminal, Frame,
+};
+
+struct App {
+    current_url: Url,
+    client: Client,
+    content: String,
+}
 
 fn main() -> Result<()> {
-    let terminal = ratatui::init();
-    let client = Client::new(true);
-    let response = client.request(Url::parse("gemini://geminiprotocol.net")?)?;
-    let mut text = String::new();
-    if let GeminiResponse::Success { mime, body } = response {
-        if mime.starts_with("text/") {
-            text = String::from_utf8_lossy(&body).into_owned();
-        }
-    } else {
-    }
-    let result = run(terminal, text);
+    let app = App::default();
+    let mut terminal = ratatui::init();
+    let result = app.run(&mut terminal);
     ratatui::restore();
     result
 }
 
-fn run(mut terminal: DefaultTerminal, text: String) -> Result<()> {
-    loop {
-        terminal.draw(|frame: &mut Frame| {
-            frame.render_widget(&text, frame.area());
-        })?;
-        if matches!(event::read()?, Event::Key(_)) {
-            break Ok(());
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            current_url: Url::parse("gemini://gemini.circumlunar.space/capcom/")
+                .expect("We know that this is a valid url"),
+            client: Client::new(true),
+            content: String::new(),
         }
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let title = Line::from(self.current_url.as_str()).bold();
+        let main_block = Block::bordered().title_top(title);
+        Paragraph::new(self.content.as_str())
+            .wrap(Wrap { trim: true })
+            .block(main_block)
+            .render(area, buf);
+    }
+}
+
+impl App {
+    pub fn run(mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        let mut first = true;
+        loop {
+            terminal.draw(|frame: &mut Frame| self.draw(frame))?;
+            if first {
+                first = false;
+                self.reload_site()?;
+            }
+            if event::poll(Duration::from_millis(300))? && matches!(event::read()?, Event::Key(_)) {
+                break Ok(());
+            }
+        }
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn reload_site(&mut self) -> Result<()> {
+        let response = self.client.request(self.current_url.clone())?;
+        match response {
+            GeminiResponse::Success { mime, body } => {
+                let content = if mime.starts_with("text") {
+                    String::from_utf8(body)?
+                } else {
+                    format!("Not type that we know how to process. {mime}")
+                };
+                self.content = content;
+            }
+            _ => unimplemented!(),
+        }
+        Ok(())
     }
 }
