a7555a09be7d55429f0dcac798f3254c9a36533f
Bruno Clemente <bruno.canguru@hotmail.com>
2025-02-20 16:09:08 -0300

    Adding text inserts

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ use url::Url;
 
 mod client;
 
-use crossterm::event::{self, Event};
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal,
+};
 use ratatui::{
+    layout::{Constraint, Layout},
     prelude::{Buffer, Rect},
-    style::{self, Color, Style, Stylize},
+    style::{Color, Style, Stylize},
     text::Line,
     widgets::{Block, Paragraph, Widget, Wrap},
     DefaultTerminal, Frame,
@@ -19,6 +23,24 @@ struct App {
     current_url: Url,
     client: Client,
     content: Option<Content>,
+    scroll: (u16, u16),
+    status: AppStatus,
+}
+
+enum AppStatus {
+    Browsing,
+    Typing(String),
+    Loading,
+}
+
+impl AppStatus {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            AppStatus::Browsing => "Browsing",
+            AppStatus::Typing(_) => "Typing",
+            AppStatus::Loading => "Loading",
+        }
+    }
 }
 
 fn main() -> Result<()> {
@@ -62,6 +84,8 @@ impl Default for App {
                 .expect("We know that this is a valid url"),
             client: Client::new(true),
             content: None,
+            scroll: (0, 0),
+            status: AppStatus::Loading,
         }
     }
 }
@@ -71,19 +95,23 @@ impl Widget for &App {
     where
         Self: Sized,
     {
+        let layout = Layout::vertical([Constraint::Min(1), Constraint::Length(1)]);
+        let [browser, command] = layout.areas(area);
         let title = Line::from(self.current_url.as_str()).bold();
         let main_block = Block::bordered().title_top(title);
         match &self.content {
-            None => Paragraph::new("No content")
-                .wrap(Wrap { trim: true })
-                .block(main_block)
-                .render(area, buf),
+            None => {
+                Paragraph::new("No content")
+                    .wrap(Wrap { trim: true })
+                    .block(main_block)
+                    .render(browser, buf);
+            }
             Some(content) => match &content.body {
                 Body::Bytes(_) => {
                     Paragraph::new("Format not supported!")
                         .wrap(Wrap { trim: true })
                         .block(main_block)
-                        .render(area, buf);
+                        .render(browser, buf);
                 }
                 Body::String(body) => {
                     if content.mime.starts_with("text/gemini") {
@@ -124,25 +152,128 @@ impl Widget for &App {
                         Paragraph::new(lines)
                             .wrap(Wrap { trim: true })
                             .block(main_block)
-                            .render(area, buf);
+                            .scroll(self.scroll)
+                            .render(browser, buf);
+                    } else {
+                        Paragraph::new(body.as_str())
+                            .wrap(Wrap { trim: true })
+                            .block(main_block)
+                            .scroll(self.scroll)
+                            .render(browser, buf);
                     }
                 }
             },
         }
+        let layout = Layout::horizontal([Constraint::Min(2), Constraint::Length(10)]);
+        let [left, right] = layout.areas(command);
+        let cmd_block = Block::new();
+        let status_block = Block::new();
+        let typed = if let AppStatus::Typing(text) = &self.status {
+            text.as_str()
+        } else {
+            ""
+        };
+        Paragraph::new(format!("=> {typed}"))
+            .block(cmd_block)
+            .render(left, buf);
+        Paragraph::new(self.status.as_str())
+            .block(status_block)
+            .render(right, buf);
     }
 }
 
 impl App {
     pub fn run(mut self, terminal: &mut DefaultTerminal) -> Result<()> {
-        let mut first = true;
         loop {
             terminal.draw(|frame: &mut Frame| self.draw(frame))?;
-            if first {
-                first = false;
-                self.reload_site()?;
+            if matches!(self.status, AppStatus::Loading) {
+                self.load_site()?;
             }
-            if event::poll(Duration::from_millis(300))? && matches!(event::read()?, Event::Key(_)) {
-                break Ok(());
+            if event::poll(Duration::from_millis(300))? {
+                match event::read()? {
+                    Event::Key(key_event) => match self.status {
+                        AppStatus::Loading => {}
+                        AppStatus::Browsing => match key_event.code {
+                            KeyCode::Esc => {
+                                break Ok(());
+                            }
+                            KeyCode::PageUp => {
+                                let step = terminal::size()?.1 - 3;
+                                self.scroll.0 = self.scroll.0.saturating_sub(step);
+                            }
+                            KeyCode::PageDown => {
+                                let step = terminal::size()?.1 - 3;
+                                self.scroll.0 = self.scroll.0.saturating_add(step);
+                            }
+                            KeyCode::Up => {
+                                self.scroll.0 = self.scroll.0.saturating_sub(1);
+                            }
+                            KeyCode::Down => {
+                                self.scroll.0 = self.scroll.0.saturating_add(1);
+                            }
+                            KeyCode::Char('i') => {
+                                self.status = AppStatus::Typing(String::new());
+                            }
+                            _ => {}
+                        },
+                        AppStatus::Typing(ref mut text) => match key_event.code {
+                            KeyCode::Esc => {
+                                self.status = AppStatus::Browsing;
+                            }
+                            KeyCode::Char(c) => {
+                                text.push(c);
+                            }
+                            KeyCode::Enter => {
+                                if let Ok(n) = text.parse::<usize>() {
+                                    let Some(Content { body, .. }) = &self.content else {
+                                        continue;
+                                    };
+                                    let Body::String(body) = body else {
+                                        continue;
+                                    };
+                                    let parser =
+                                        GemTextParser::new(&body, self.current_url.clone());
+                                    let Some(link) = parser
+                                        .flatten()
+                                        .filter_map(|line| match line {
+                                            GemTextLine::Link { url, .. } => Some(url),
+                                            _ => None,
+                                        })
+                                        .enumerate()
+                                        .filter_map(
+                                            |(n_link, link)| {
+                                                if n_link == n {
+                                                    Some(link)
+                                                } else {
+                                                    None
+                                                }
+                                            },
+                                        )
+                                        .next()
+                                    else {
+                                        continue;
+                                    };
+                                    self.current_url = link;
+                                    self.status = AppStatus::Loading;
+                                    self.content = None;
+                                    continue;
+                                }
+                                if text.starts_with("gemini://") {
+                                    self.current_url = Url::parse(text)?;
+                                    self.status = AppStatus::Loading;
+                                    self.content = None;
+                                    continue;
+                                }
+                                self.current_url = self.current_url.join(&text)?;
+                                self.status = AppStatus::Loading;
+                                self.content = None;
+                            }
+                            _ => {}
+                        },
+                    },
+
+                    _ => {}
+                }
             }
         }
     }
@@ -151,7 +282,7 @@ impl App {
         frame.render_widget(self, frame.area());
     }
 
-    fn reload_site(&mut self) -> Result<()> {
+    fn load_site(&mut self) -> Result<()> {
         let response = self.client.request(self.current_url.clone())?;
         match response {
             GeminiResponse::Success { mime, body } => {
@@ -159,6 +290,7 @@ impl App {
             }
             _ => unimplemented!(),
         }
+        self.status = AppStatus::Browsing;
         Ok(())
     }
 }
@@ -190,9 +322,9 @@ impl<'a> GemTextParser<'a> {
             line
         };
         if let Some(link_line) = line.strip_prefix("=> ") {
-            let Some((link, text)) = link_line.split_once(|x: char| x.is_whitespace()) else {
-                bail!("Invalid link line {line}");
-            };
+            let (link, text) = link_line
+                .split_once(|x: char| x.is_whitespace())
+                .unwrap_or((link_line, ""));
             if !link.contains("://") {
                 return Ok(GemTextLine::Link {
                     url: self.url.join(link)?,
